65296c412607035fa53a9bcad519291cf57a94a5
Ilona Brazauskaitė <ilona.brazauskaite@gmail.com>
2020-03-03 11:18:42 +0200

    DB pirminis duomenų rinkinys

---
+++ DarbuotojuDB/dbo/Tables/dbo.Darbuotojas.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Darbuotojas] ON
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (1, N'Ilona', N'Brazauskaitė', N'49702042260', N'1997-02-04', N'Vakaro g. 18, Dainių km.', 1)
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (2, N'Jonas', N'Jonaitis', N'39110231462', N'1991-10-23', N'Partizanu g. 29-21', 0)
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (3, N'Petras', N'Petraitis', N'38603243954', N'1986-03-24', N'Geležinio vilko g. 92-47', 1)
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (4, N'Tomas', N'Tomaitis', N'39012122255', N'1990-12-12', N'P. Lukšio 29-14', 1)
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (5, N'Sigita', N'Sigutė', N'49204081456', N'1992-04-08', N'Saulės g. 58-2', 0)
+INSERT INTO [dbo].[Darbuotojas] ([Id], [Vardas], [Pavarde], [Asmens_kodas], [Gimimo_data], [Adresas], [Aktyvumo_pozymis]) VALUES (6, N'Vilija', N'Vilimaitė', N'49508041648', N'1995-08-04', N'Radvilėnų pl. 34', 1)
+SET IDENTITY_INSERT [dbo].[Darbuotojas] OFF
